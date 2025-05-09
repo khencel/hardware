fdd2b8d739a992601c2a8601a1f9180b4802cfa9
zhacked <fsiapco@gmail.com>
2025-05-09 17:33:33 +0800

    fix: change dollar icon to peso

--- resources/views/pos/pos_order.blade.php
+++ resources/views/pos/pos_order.blade.php
@@ -462,7 +462,7 @@
                         <td>₱${item.price.toFixed(2)}</td>
                         <td class="quantity-cell">
                             <button class="quantity-btn minus" data-id="${item.id}">-</button>
-                            <span class="quantity-value"> ₱{item.quantity}</span>
+                            <span class="quantity-value"> ₱${item.quantity}</span>
                             <button class="quantity-btn plus" data-id="${item.id}">+</button>
                         </td>
                         <td>₱${total.toFixed(2)}</td>
@@ -818,7 +818,7 @@
                             <span class="item-quantity">${item.quantity}x</span>
                             <span>${item.name}</span>
                         </div>
-                        <div class="item-total">$${itemTotal.toFixed(2)}</div>
+                        <div class="item-total"> ₱${itemTotal.toFixed(2)}</div>
                     </div>
                 `;
             });
